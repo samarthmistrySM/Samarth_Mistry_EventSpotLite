147fade421fe0468259fd6d273b49d2ccc0a57ca
Samarth Mistry <g33ky.sam@gmail.com>
2024-10-27 23:16:24 +0530

    ui update

--- src/components/LikedEvents.jsx
+++ src/components/LikedEvents.jsx
@@ -3,7 +3,7 @@ import EventCard from "./EventCard";
 
 const LikedEvents = ({ likedPosts, openModal, handleLikePost }) => {
   return (
-    <div className="p-6 min-h-[85vh] bg-white dark:bg-gray-900">
+    <div className="p-6 min-h-[90vh] bg-white dark:bg-gray-900">
       <h2 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">Liked Events</h2>
       {likedPosts.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
